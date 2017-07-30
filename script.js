2d1202bf4caa7538949e1c8812808f9e5efbfc89
Juliette-C <juliette.coustard@gmail.com>
2017-07-30 15:31:42 +0200

    Buildings : creation of class Buildings and list of constructed buildings in an array

--- script.js
+++ script.js
@@ -1,24 +1,86 @@
 const widthMap = 5;
 const heightMap = 5;
 
-const buildingsArray = [
-    {id: 'farm', class: 'building', name: 'Farm', image: 'building_farm.svg'},
-    {id: 'hunter', class: 'building', name: 'Hunter', image: 'building_hunter.svg'},
-    {id: 'fishery', class: 'building', name: 'Fishery', image: 'building_fishery.svg'},
-    {id: 'windmill', class: 'building', name: 'Windmill', image: 'building_windmill.svg'},
-    {id: 'warehouse', class: 'building', name: 'Warehouse', image: 'building_warehouse.svg'},
+class Building {
+    constructor(id, name, description, image, people, resourcesProd, resourcesUse, resourcesConstruction, resourcesStorageMax) {
+        this.id = id;
+        this.name = name;
+        this.descr = description;
+        this.image = image;
+        this.people = people;
+        this.resourcesProd = resourcesProd;
+        this.resourcesUse = resourcesUse;
+        this.resourcesStorageMax = resourcesStorageMax;
+    }
+}
+
+const farmer = new Building('farmer', 'Farmer', 'Products food', 'building_farmer.svg', 4, 
+    {food: 2, log: 0, stone: 0, happiness: 0}, // prod
+    {food: 2, log: 0, stone: 0, happiness: 1}, // use
+    {food: 0, log: 2, stone: 0, happiness: 0}, // constr
+    {food: 8, log: 0, stone: 0, happiness: 8} // storage
+);
+
+const hunter = new Building('hunter', 'Hunter', 'Products food', 'building_hunter.svg', 4, 
+    {food: 2, log: 0, stone: 0, happiness: 0}, // prod
+    {food: 2, log: 0, stone: 0, happiness: 1}, // use
+    {food: 0, log: 2, stone: 0, happiness: 0}, // constr
+    {food: 8, log: 0, stone: 0, happiness: 8} // storage
+);
+
+const woodcutter = new Building('woodcutter', 'Woodcutter', 'Products logs', 'building_woodcutter.svg', 4, 
+    {food: 0, log: 3, stone: 0, happiness: 0}, // prod
+    {food: 2, log: 0, stone: 0, happiness: 1}, // use
+    {food: 0, log: 2, stone: 0, happiness: 0}, // constr
+    {food: 4, log: 6, stone: 0, happiness: 8} // storage
+);
+
+const stonecutter = new Building('stonecutter', 'Stonecutter', 'Products stones', 'building_stonecutter.svg', 4, 
+    {food: 0, log: 0, stone: 3, happiness: 0}, // prod
+    {food: 2, log: 0, stone: 0, happiness: 1}, // use
+    {food: 0, log: 2, stone: 0, happiness: 0}, // constr
+    {food: 4, log: 0, stone: 6, happiness: 8} // storage
+);
+
+const warehouse = new Building('warehouse', 'Warehouse', 'Storage local', 'building_warehouse.svg', 0, 
+    {food: 0, log: 0, stone: 0, happiness: 0}, // prod
+    {food: 0, log: 0, stone: 0, happiness: 0}, // use
+    {food: 0, log: 2, stone: 2, happiness: 0}, // constr
+    {food: 20, log: 20, stone: 20, happiness: 8} // storage
+);
+
+const townHall = {
+    id: 'townHall',
+    name: 'TownHall',
+    descr: 'Displays city datas and allows trade',
+    image: 'building_townhall.svg',
+    resourcesProd: {food: 0, log: 10, stone: 10, happiness: 0}
+}
+
+const buildingsArray = [farmer, hunter, woodcutter, stonecutter, warehouse, townHall];
+
+const toolsArray = [
     {id: 'removeTool', class: 'tool', name: 'Remove', image: 'tool_broom.svg'}
-];
+]
+
+var constructedBuildingsArray = Array(widthMap * heightMap).fill(null);
 
 /* ----------------------------------- Initial Display ----------------------------------- */
 
-/* Buildings list */
+/* Tool Bar */
 const $buildingList = document.getElementById('buildingsList');
 $buildingList.innerHTML = buildingsArray
-    .map( building => `<div id="${building.id}" class="selectable ${building.class}">
-        <img src="images/${building.image}" alt="${building.name}" title="${building.name}">
+    .map( building => `<div id="${building.id}" class="selectable building">
+        <img src="images/${building.image}" alt="${building.name}" title="${building.descr}">
         </div>`)
     .join('');
+
+const $toolsList = document.getElementById('toolsList');
+$toolsList.innerHTML = toolsArray
+    .map( tool => `<div id ="${tool.id}" class="selectable toll">
+        <img src="images/${tool.image}" alt="${tool.name}" title="${tool.descr}">
+        </div>`)
+        .join('');
 
 /* Map tiles */
 const $map = document.getElementById('map');
@@ -32,6 +94,7 @@ $map.innerHTML = Array(totalTiles).fill(1)
 let displayedBuildingsList = document.getElementsByClassName('selectable');
 let displayedTiles = document.getElementsByClassName('tile');
 let idConstructedBuilding;
+let idChosenTile;
 
 for(let building of displayedBuildingsList) {
     building.addEventListener('click', function(event) {
@@ -43,19 +106,33 @@ for(let building of displayedBuildingsList) {
 for(let tile of displayedTiles) {
     tile.addEventListener('click', function(event){
         event.stopPropagation();
+        idChosenTile = tile.id.split('_')[1];
+
         if(!idConstructedBuilding) return;
+
+        // Demolition
         if (idConstructedBuilding === "removeTool") {
-            if(window.confirm('Remove this building?')) tile.innerHTML = "";
-        } else {
+            if(tile.innerHTML != "" && window.confirm('Remove this building?')) {
+                tile.innerHTML = '';
+                constructedBuildingsArray[idChosenTile] = null;
+            }
+        }
+        
+        // Construction
+        else {
             chosenBuilding = buildingsArray.find( building => (building.id == idConstructedBuilding));
             
             if (!chosenBuilding) return;
-            if(tile.innerHTML != "") {
-                alert('This case is not empty. Remove');
+            if(constructedBuildingsArray[idChosenTile] != null) {
+                alert('This case is not empty. Remove actual building.');
                 return;
-            } 
+            }
+            constructedBuildingsArray[idChosenTile] = chosenBuilding;
             tile.innerHTML = `<img src="images/${chosenBuilding.image}" alt="${chosenBuilding.name}" title="${chosenBuilding.name}">`;
+            
         }
+
+        console.log(constructedBuildingsArray);
     })
 }
 
