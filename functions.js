6c0df55ee1aeae74b013415d8ecd3248be155a05
Juliette-C <juliette.coustard@gmail.com>
2017-08-10 18:13:11 +0200

    Display : improved informations display - JS : improved automatic insertions

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 const widthMap = 6; // data in .css too
 const heightMap = 6; // data in .css too
-const frequencyDisplay = 3000;
+const frequencyDisplay = 4000;
 const nbBackgroundTiles = 4;
 
 const $toolsList = document.getElementById('toolsList');
@@ -8,23 +8,38 @@ const $informationsSection = document.getElementById('informationsSection');
 const $map = document.getElementById('map');
 const $informationsCityBar = document.getElementById('informationsCity');
 
+// const logos = {
+//     people: 'images/logo_people.svg',
+//     food: 'images/logo_food.svg',
+//     log: 'images/logo_log.svg',
+//     stone: 'images/logo_stone.svg',
+//     happiness: 'images/logo_happiness.png'
+// }
+
+const logos = {
+    people: {image: 'images/logo_people.svg', alt: 'people', title: 'People'},
+    food: {image: 'images/logo_food.svg', alt: 'food', title: 'Food'},
+    log: {image: 'images/logo_log.svg', alt: 'log', title: 'Log'},
+    stone: {image: 'images/logo_stone.svg', alt: 'log', title: 'Log'},
+    happiness: {image: 'images/logo_happiness.png', alt: 'log', title: 'Log'}
+}
 
 // -----------------------------------------------------------------------------
 // -------------------------------- Game datas ---------------------------------
 // -----------------------------------------------------------------------------
 
 class Building {
-    constructor(id, name, description, image, people, resourcesProd, resourcesUse, resourcesConstruction, resourcesStorageMax) {
+    constructor(id, name, description, image, people, production, use, construction, storage) {
         this.id = id;
         this.type = 'building',
         this.name = name;
         this.descr = description;
-        this.image = image;
+        this.image = 'images/' + image;
         this.people = people;
-        this.resourcesProd = resourcesProd;
-        this.resourcesUse = resourcesUse;
-        this.resourcesConstruction = resourcesConstruction;
-        this.resourcesStorageMax = resourcesStorageMax;
+        this.production = production;
+        this.use = use;
+        this.construction = construction;
+        this.storage = storage;
     }
 }
 
@@ -34,7 +49,7 @@ class Tools {
         this.type = 'tool';
         this.name = name;
         this.descr = description;
-        this.image = image;
+        this.image = 'images/' + image;
     }
 }
 
@@ -81,7 +96,7 @@ const circus = new Building('circus', 'Circus', 'Products happiness', 'building_
     {food: 0, log: 0, stone: 0, happiness: 0} // storage
 );
 
-const townHall = new Building('townHall', 'TownHall', 'Displays city datas and allows trade', 'building_townhall.svg', 0,
+const townHall = new Building('townHall', 'TownHall', 'Displays city datas', 'building_townhall.svg', 0,
     {food: 0, log: 0, stone: 0, happiness: 0}, // prod
     {food: 0, log: 0, stone: 0, happiness: 0}, // use
     {food: 0, log: 10, stone: 10, happiness: 0}, // constr
@@ -96,34 +111,124 @@ const buildingsArray = [farmer, hunter, woodcutter, stonecutter, warehouse, town
 const toolsArray = [broomRemove];
 var constructedBuildingsArray = Array(widthMap * heightMap).fill(null);
 
-var initialDatas = {people: 0, food: 2, log: 4, stone: 0, happiness: 0};
-var actualDatas = {};
+var dataCity = {people: 0, food: 2, log: 4, stone: 0, happiness: 0};
 
-// ----- Functions -----
-function upDateDatas(datas, delta){    
-    if(Object.keys(datas).length === 0)
-        datas = initialDatas;
+// -----------------------------------------------------------------------------
+// ---------------------------------  Display ----------------------------------
+// -----------------------------------------------------------------------------
 
-    // ----- production -----
-    const production = constructedBuildingsArray.filter(building => building != null)
-        .reduce((resourcesProducted, building) => {
-            Object.keys(building.resourcesProd).forEach(key => resourcesProducted[key] += building.resourcesProd[key]);
-            return resourcesProducted;
-        }, {food: 0, log: 0, stone: 0, happiness: 0});
+function displayToolsList() {
+    // ----- tools -----
+    $toolsList.innerHTML += toolsArray
+        .map( tool => `<div id ="${tool.id}" class="selectableTool ${tool.type}">
+            <img src="${tool.image}" alt="${tool.name}" title="${tool.descr}">
+            </div>`)
+            .join('');
 
-    // ----- use -----
-    const use = constructedBuildingsArray.filter(building => building != null)
-        .reduce((resourcesUsed, building) => {
-            Object.keys(building.resourcesUse).forEach(key => resourcesUsed[key] += building.resourcesUse[key]);
-            return resourcesUsed;
-        }, {food: 0, log: 0, stone: 0, happiness: 0});
+    // ----- buildings -----
+    $toolsList.innerHTML += buildingsArray
+        .map( building => `<div id ="${building.id}" class="selectableTool ${building.type}">
+            <img src="${building.image}" alt="${building.name}" title="${building.descr}">
+            </div>`)
+            .join('');
+}
 
-    // ----- calculation -----
-    Object.keys(production).forEach( key => {
-        datas[key] += delta * (production[key] - use[key])
-    });
+function displayMap() {
+    // random backgrounds
+    $map.innerHTML = Array(widthMap * heightMap).fill(1)
+        .map((n, i) => `<div id="tile_${i}" class="tileMap tileBackground_${Math.trunc((Math.random()*100) % nbBackgroundTiles)}"></div>`)
+        .join('');
+}
 
-    return datas;
+function displayInformationsCity(cityDatas) {
+    let data = cityDatas;
+    Object.keys(data).forEach( key => data[key] = Math.trunc(data[key]) );
+
+    let prod = calculateResourcesProd();
+    Object.keys(prod).forEach( key => prod[key] = Math.trunc(prod[key]) );
+
+    let used = calculateResourcesUse();
+    Object.keys(used).forEach( key => used[key] = -Math.trunc(used[key]) );
+
+    let sum = {};
+    Object.keys(prod).forEach( key  => sum[key] = prod[key] + used[key] );
+
+    $informationsCityBar.innerHTML = `<p id="totalPeople"><img src="${logos.people.image}" alt="${logos.people.alt}" title="${logos.people.title}"> ${data.people}</p>`;
+    Object.keys(prod).forEach( key => {
+        $informationsCityBar.innerHTML += `<div class="catInfosCityBar">
+            <p><img src="${logos[key].image}" alt="${logos[key].alt}" title="${logos[key].title}"> ${data[key]} (${displaySignedNumber(sum[key])})</p>
+            <div class="details"><p>${displaySignedNumber(prod[key])}</p><p>${displaySignedNumber(used[key])}</p></div>
+            </div>`;
+        }
+    );
+}
+
+function displayInitialInformationsCity(){
+    displayInformationsCity(dataCity);
+}
+
+function displayInformationsTool(idReadTool) {
+    var chosenObject = toolsArray.find( tool => (tool.id == idReadTool));
+    if(!chosenObject)
+        var chosenObject = buildingsArray.find( building => (building.id == idReadTool));
+    if (!chosenObject)
+        return;
+
+    if (chosenObject.type === 'tool') {
+        $informationsSection.innerHTML = `<p class="infosToolTitle">${chosenObject.name}</p>
+        <p class="infosToolText">${chosenObject.descr}</p>`;
+    }
+    else if (chosenObject.type === 'building'){
+        addContentInformationsSection(chosenObject);
+    }
+}
+
+function addContentInformationsSection(chosenObject) {
+    $informationsSection.innerHTML = 
+        `<p class="infosToolTitle">${chosenObject.name}</p>
+
+        <p class="infosToolText">${chosenObject.descr}<br />
+        People : ${chosenObject.people} <img src="${logos.people.image}" alt="${logos.people.alt}" title="${logos.people.title}"></p>
+
+        <p class="infosToolSubTitle">Construction needs :</p>
+        <p class="infosToolText"><img src="${logos.food.image}" alt="${logos.food.alt}" title="${logos.food.title}">${chosenObject.construction.food} <img src="${logos.log.image}" alt="${logos.log.alt}" title="${logos.log.title}">${chosenObject.construction.log} <img src="${logos.stone.image}" alt="${logos.stone.alt}" title="${logos.stone.title}">${chosenObject.construction.stone} <img src="${logos.happiness.image}" alt="${logos.happiness.alt}" title="${logos.happiness.title}">${chosenObject.construction.happiness}</p>
+
+        <p class="infosToolSubTitle">Construction products :</p>
+        <p class="infosToolText"><img src="${logos.food.image}" alt="${logos.food.alt}" title="${logos.food.title}">${chosenObject.production.food} <img src="${logos.log.image}" alt="${logos.log.alt}" title="${logos.log.title}">${chosenObject.production.log} <img src="${logos.stone.image}" alt="${logos.stone.alt}" title="${logos.stone.title}">${chosenObject.production.stone} <img src="${logos.happiness.image}" alt="${logos.happiness.alt}" title="${logos.happiness.title}">${chosenObject.production.happiness}</p>`;
+}
+
+function displayInformationsTile(idTile) {
+    // building on the tile ?
+    let selectedBuilding = constructedBuildingsArray[idTile];
+    if(!selectedBuilding) return;
+
+    // what building ?
+    let displayedBuilding = buildingsArray.find( building => (building.id == selectedBuilding.id));
+    if(!displayedBuilding) return;
+
+    // display informations
+    addContentInformationsSection(displayedBuilding);
+}
+
+function deleteInformationsTool() {
+    $informationsSection.innerHTML = '';
+}
+
+let lastTime = 0;
+function updateDisplayPerFrequency(time){
+    const delta = (time - lastTime) / frequencyDisplay;
+
+    dataCity = upDateDatas(dataCity, delta);
+    displayInformationsCity(dataCity);
+
+    lastTime = time;
+    setTimeout( () => 
+        requestAnimationFrame((time) => updateDisplayPerFrequency(time))
+    , frequencyDisplay);
+}
+
+function displaySignedNumber(n) {
+    return n>0 ? '+' + n : n;
 }
 
 // -----------------------------------------------------------------------------
@@ -148,8 +253,8 @@ function constructBuilding(tile, idChosenTile, idChosenTool){
 
     // ----- available resources ? -----
     let possibleConstruction = true;
-    Object.keys(chosenBuilding.resourcesConstruction).forEach ( key => {
-        if(chosenBuilding.resourcesConstruction[key] > 0 && chosenBuilding.resourcesConstruction[key] > actualDatas[key]) {
+    Object.keys(chosenBuilding.construction).forEach ( key => {
+        if(chosenBuilding.construction[key] > 0 && chosenBuilding.construction[key] > dataCity[key]) {
             alert('More resources are needed...');
             possibleConstruction = false;
         }}
@@ -158,121 +263,66 @@ function constructBuilding(tile, idChosenTile, idChosenTool){
     // construction
     if(possibleConstruction) {
         constructedBuildingsArray[idChosenTile] = chosenBuilding;
-        tile.innerHTML = `<img class="buildingOnTileMap" src="images/${chosenBuilding.image}" alt="${chosenBuilding.name}" title="${chosenBuilding.name}">`;
+        tile.innerHTML = `<img class="buildingOnTileMap" src="${chosenBuilding.image}" alt="${chosenBuilding.name}" title="${chosenBuilding.name}">`;
         
         // update datas
-        Object.keys(chosenBuilding.resourcesConstruction).forEach( key =>
-            actualDatas[key] -= chosenBuilding.resourcesConstruction[key]
+        Object.keys(chosenBuilding.construction).forEach( key =>
+            dataCity[key] -= chosenBuilding.construction[key]
         );
-        actualDatas.people += chosenBuilding.people;
+        dataCity.people += chosenBuilding.people;
 
-        displayInformationsCity(actualDatas);
+        displayInformationsCity(dataCity);
     }
 }
 
 function removeBuilding(tile, idChosenTile){
     if(tile.innerHTML != "" && window.confirm('Remove this building?')) {
         tile.innerHTML = '';
+        dataCity.people -= constructedBuildingsArray[idChosenTile].people;
         constructedBuildingsArray[idChosenTile] = null;
-        console.log(constructedBuildingsArray[idChosenTile]);
     }
 }
 
 
 // -----------------------------------------------------------------------------
-// ---------------------------------  Display ----------------------------------
+// -------------------------------  Calculation --------------------------------
 // -----------------------------------------------------------------------------
 
-function displayToolsList() {
-    // ----- tools -----
-    $toolsList.innerHTML += toolsArray
-        .map( tool => `<div id ="${tool.id}" class="selectableTool ${tool.type}">
-            <img src="images/${tool.image}" alt="${tool.name}" title="${tool.descr}">
-            </div>`)
-            .join('');
+function calculateResourcesProd() {
+    let resourcesProd = {food: 0, log: 0, stone: 0, happiness: 0};
 
-    // ----- buildings -----
-    $toolsList.innerHTML += buildingsArray
-        .map( building => `<div id ="${building.id}" class="selectableTool ${building.type}">
-            <img src="images/${building.image}" alt="${building.name}" title="${building.descr}">
-            </div>`)
-            .join('');
+    resourcesProd = constructedBuildingsArray
+                    .filter(building => building != null)
+                    .reduce((prod, building) => {
+                        Object.keys(resourcesProd).forEach(key => prod[key] += building.production[key]);
+                        return prod;
+                    }, resourcesProd);
+    
+    return resourcesProd;
 }
 
-function displayMap() {
-    // random backgrounds
-    $map.innerHTML = Array(widthMap * heightMap).fill(1)
-        .map((n, i) => `<div id="tile_${i}" class="tileMap tileBackground_${Math.trunc((Math.random()*100) % nbBackgroundTiles)}"></div>`)
-        .join('');
+function calculateResourcesUse() {
+    let resourcesUsed = {food: 0, log: 0, stone: 0, happiness: 0};
+
+    resourcesUsed = constructedBuildingsArray
+                    .filter(building => building != null)
+                    .reduce((used, building) => {
+                        Object.keys(resourcesUsed).forEach(key => used[key] += building.use[key]);
+                        return used;
+                    }, resourcesUsed);
+    
+    return resourcesUsed;
 }
 
-function displayInformationsCity(cityDatas) {
-    console.log(cityDatas);
-    $informationsCityBar.innerHTML = `<p id="totalPeople"><img src="images/logo_population.svg" alt="population" title="Population"> ${Math.trunc(cityDatas.people)}</p>
-    <p id="totalFood"><img src="images/logo_food.svg" alt="food" title="Food"> ${Math.trunc(cityDatas.food)}</p>
-    <p id="totalLog"><img src="images/logo_log.svg" alt="log" title="Log"> ${Math.trunc(cityDatas.log)}</p>
-    <p id="totalStone"><img src="images/logo_stone.svg" alt="stone" title="Stone"> ${Math.trunc(cityDatas.stone)}</p>
-    <p id="totalHappiness"><img src="images/logo_happiness.png" alt="happiness" title="Happiness">${Math.trunc(cityDatas.happiness)}</p>`;
-}
+function upDateDatas(datas, delta){
+    if(Object.keys(datas).length === 0)
+        datas = dataCity;
 
-function displayInitialInformationsCity(){
-    displayInformationsCity(initialDatas);
-}
+    let resourcesProd = calculateResourcesProd();
+    let resourcesUsed = calculateResourcesUse();
 
-function addContentInformationsSection(chosenObject) {
-    $informationsSection.innerHTML = 
-        `<p class="infosToolTitle">${chosenObject.name}</p>
-        <p class="infosToolText">${chosenObject.descr}</p>
-        <p class="infosToolSubTitle">Construction needs :</p>
-        <p class="infosToolText"><img src="images/logo_food.svg" alt="food">${chosenObject.resourcesConstruction.food} <img src="images/logo_log.svg" alt="log">${chosenObject.resourcesConstruction.log} <img src="images/logo_stone.svg" alt="stone">${chosenObject.resourcesConstruction.stone} <img src="images/logo_happiness.png" alt="happiness">${chosenObject.resourcesConstruction.happiness}</p>
-        <p class="infosToolSubTitle">Construction products :</p>
-        <p class="infosToolText"><img src="images/logo_food.svg" alt="food">${chosenObject.resourcesProd.food} <img src="images/logo_log.svg" alt="log">${chosenObject.resourcesProd.log} <img src="images/logo_stone.svg" alt="stone">${chosenObject.resourcesProd.stone} <img src="images/logo_happiness.png" alt="happiness">${chosenObject.resourcesProd.happiness}</p>`;
-}
-
-function displayInformationsTool(idReadTool) {
-    var chosenObject = toolsArray.find( tool => (tool.id == idReadTool));
-    if(!chosenObject)
-        var chosenObject = buildingsArray.find( building => (building.id == idReadTool));
-    if (!chosenObject)
-        return;
-
-    if (chosenObject.type === 'tool') {
-        $informationsSection.innerHTML = `<p class="infosToolTitle">${chosenObject.name}</p>
-        <p class="infosToolText">${chosenObject.descr}</p>`;
-    }
-    else if (chosenObject.type === 'building'){
-        addContentInformationsSection(chosenObject);
-    }
-}
-
-function displayInformationsTile(idTile) {
-    // building on the tile ?
-    let selectedBuilding = constructedBuildingsArray[idTile];
-    if(!selectedBuilding) return;
-
-    // what building ?
-    let displayedBuilding = buildingsArray.find( building => (building.id == selectedBuilding.id));
-    if(!displayedBuilding) return;
-
-    // display informations
-    addContentInformationsSection(displayedBuilding);
-}
-
-function deleteInformationsTool() {
-    $informationsSection.innerHTML = '';
-}
-
-// ----- update display -----
-let lastTime = 0;
-
-function updateDisplayPerFrequency(time){
-    const delta = (time - lastTime) / frequencyDisplay;
-
-    actualDatas = upDateDatas(actualDatas, delta);
-    displayInformationsCity(actualDatas);
-
-    lastTime = time;
-    setTimeout( () => 
-        requestAnimationFrame((time) => updateDisplayPerFrequency(time))
-    , frequencyDisplay);
+    Object.keys(resourcesProd).forEach( key => {
+        datas[key] += delta * (resourcesProd[key] - resourcesUsed[key])
+    });
+    return datas;
 }
